a413b19b1de577be60aba140ba834ab6d1887847
Akshay1000101 <91070037+Akshay1000101@users.noreply.github.com>
2023-08-21 14:38:15 +0530

    Delete max and min signed number directory

--- max and min signed number/int.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-#include<math.h>
-int main(){
-long long int max = (long long int) (pow(2,63) -1);
-long long int min = (long long int) (pow(2,63) *-1);
-printf("lowest no represented by signed 64bit int is  %lld\n",min);
-printf("highest no represented by signed 64bit int is  %lld\n",max);
-return 0;}
